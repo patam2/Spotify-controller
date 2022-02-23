1d3c1ba135916fca716e63986afcfdf8c91748bc
0 <88111864+patam2@users.noreply.github.com>
2022-02-24 00:39:00 +0200

    reset socket

--- core/http_sockets.py
+++ core/http_sockets.py
@@ -2,16 +2,20 @@ import socket
 import ssl
 import json
 
+socket.setdefaulttimeout(5)
+
 
 def CallBack():
     sock = socket.socket()
-    sock.bind(('localhost', 8888))
+    sock.bind(("localhost", 8888))
     sock.listen()
     c, a = sock.accept()
     recv = c.recv(1024)
 
-    return recv.split(b'/callback?code=')[1].split(b' HTTP/1.1')[0].decode()
+    return recv.split(b"/callback?code=")[1].split(b" HTTP/1.1")[0].decode()
 
+
+# Todo: proper error & false response handling
 class SocketClient:
     def __init__(self, token: str, host: str) -> None:
         self.host = host
@@ -21,10 +25,15 @@ class SocketClient:
     def _apply_headers(self, method, data):
         return (
             f"Host: {self.host}\r\n"
-            f'Authorization: {self.token}\r\n'
+            f"Authorization: {self.token}\r\n"
             "Accept: application/json\r\n"
             "Content-Type: %s"
-            f"Content-Length: {0 if not data else len(json.dumps(data))}\r\n" % ("application/x-www-form-urlencoded\r\n" if method == 'POST' and self.host == 'accounts.spotify.com' else 'application/json\r\n')
+            f"Content-Length: {0 if not data else len(json.dumps(data))}\r\n"
+            % (
+                "application/x-www-form-urlencoded\r\n"
+                if method == "POST" and self.host == "accounts.spotify.com"
+                else "application/json\r\n"
+            )
         )
 
     def create_sock(self):
@@ -38,31 +47,38 @@ class SocketClient:
         sock.connect((self.host, 443))
         return sock
 
-    def request(self, method, url, data=None):
-        self.socket_sess.sendall(
-            str.encode(
-                "%s %s HTTP/1.1\r\n%s\r\n%s\r\n\r\n"
-                % (method, url, self._apply_headers(method, data), (json.dumps(data) if data else ""))
-            )
-        )
-
-        data, recv = self.socket_sess.recv(4096).split(b'\r\n\r\n', 1)
-
-        content_length = self._get_content_length(data)
-        while len(recv) < content_length:
-            recv += self.socket_sess.recv(4096)
-
-        if b'HTTP/1.1 303' in data:
-            return data.split(b'location: ')[1].split(b'\r\nset-cookie')[0]
-
+    def request(self, method, url, sent_data=None):
         try:
-            return json.loads(recv.decode()) if recv else True
-        except:
-            return False
+            self.socket_sess.sendall(
+                str.encode(
+                    "%s %s HTTP/1.1\r\n%s\r\n%s\r\n\r\n"
+                    % (
+                        method,
+                        url,
+                        self._apply_headers(method, sent_data),
+                        (json.dumps(sent_data) if sent_data else ""),
+                    )
+                )
+            )
+
+            data, recv = self.socket_sess.recv(4096).split(b"\r\n\r\n", 1)
+            content_length = self._get_content_length(data)
+            while len(recv) < content_length:
+                recv += self.socket_sess.recv(4096)
+            if b"HTTP/1.1 303" in data:
+                return data.split(b"location: ")[1].split(b"\r\nset-cookie")[0]
+            try:
+                return json.loads(recv.decode()) if recv else True
+            except:
+                return False
+        except ConnectionResetError:
+            print("Conn most likely failed; retrying")
+            self.__init__(self.token, self.host)
+            return self.request(method, url, sent_data)
 
     def _get_content_length(self, data):
         data = data.lower()
-        if b'content-length' in data:
-            return int(data.split(b'content-length: ')[1].split(b'\r\n')[0])
+        if b"content-length" in data:
+            return int(data.split(b"content-length: ")[1].split(b"\r\n")[0])
         else:
             return 0
