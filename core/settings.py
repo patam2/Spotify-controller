8707fcc75b06274157a20a57f36efef8adec3e16
0 <88111864+patam2@users.noreply.github.com>
2022-02-24 11:57:26 +0200

    lesser code smell

--- core/settings.py
+++ core/settings.py
@@ -4,7 +4,7 @@ from core import kboard
 
 parentdir = os.path.join(os.path.dirname(__file__) + "/../")
 
-#todo - write class?
+#todo - write as class?
 def get_settings():
     settings = {}
     if "settings.json" not in os.listdir(parentdir):
@@ -15,9 +15,9 @@ def get_settings():
         settings['refresh_token'] = ''
         settings['keybind'] = kboard.listen_for_hotkey()
         return settings
-    else: #settings found
-        settings = json.loads(open(f'{parentdir}/settings.json').read())
-        return settings
+
+    settings = json.loads(open(f'{parentdir}/settings.json').read())
+    return settings
 
 def save_settings(settings):
     set_file = open(f'{parentdir}/settings.json', 'w')
--- spot.py
+++ spot.py
@@ -6,14 +6,16 @@ spot = spotify.Spotify()
 
 
 def change_playback():
-    currently_playing = spot.get_player()
-    if "device" not in currently_playing:
-        print("Spotify not found")
-        return
-    currently_playing = not currently_playing["is_playing"]
-    spot.play_music(currently_playing)
-    print("Paused track" if not currently_playing else "Resuming track")
-
+    try:
+        currently_playing = spot.get_player()
+        if "device" not in currently_playing:
+            print("Spotify not found")
+            return
+        currently_playing = not currently_playing["is_playing"]
+        spot.play_music(currently_playing)
+        print("Paused track" if not currently_playing else "Resuming track")
+    except Exception as Error:
+        print(Error, 'happened')
 
 print("Press", spot.settings["keybind"], "to pause/play")
 
