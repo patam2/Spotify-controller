ce8216000e65fa23d4a331c390d7b5e006d0a1f9
0 <88111864+patam2@users.noreply.github.com>
2022-02-24 12:51:42 +0200

    Update spot.py

--- spot.py
+++ spot.py
@@ -3,16 +3,17 @@ from core import spotify
 from core.player import SpotifyPlayer
 
 
-spot = spotify.Spotify()
-player = SpotifyPlayer(spot)
+if __name__ == "__main__":
+    spot = spotify.Spotify()
+    player = SpotifyPlayer(spot)
 
 
-print("Press", spot.settings["keybind_pause"], "to pause/play")
-print("Press", spot.settings['keybind_skip'], 'to skip the song')
-print("Press", spot.settings['keybind_previous'], 'to play the previous song\n')
+    print("Press", spot.settings["keybind_pause"], "to pause/play")
+    print("Press", spot.settings['keybind_skip'], 'to skip the song')
+    print("Press", spot.settings['keybind_previous'], 'to play the previous song\n')
 
 
-keyboard.add_hotkey(spot.settings["keybind_pause"], player.change_playback, suppress=True)
-keyboard.add_hotkey(spot.settings["keybind_skip"], player.skip_song, suppress=True)
-keyboard.add_hotkey(spot.settings["keybind_previous"], player.previous_song, suppress=True)
-keyboard.wait()
+    keyboard.add_hotkey(spot.settings["keybind_pause"], player.change_playback, suppress=True)
+    keyboard.add_hotkey(spot.settings["keybind_skip"], player.skip_song, suppress=True)
+    keyboard.add_hotkey(spot.settings["keybind_previous"], player.previous_song, suppress=True)
+    keyboard.wait()
